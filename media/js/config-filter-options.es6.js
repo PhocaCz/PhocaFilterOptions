220fc121e0a92272c4efac063a3a211de8619313
Jan <PhocaCz@users.noreply.github.com>
2021-08-31 17:23:54 +0200

    4.0.3
    4.0.3

--- media/js/config-filter-options.es6.js
+++ media/js/config-filter-options.es6.js
@@ -1,52 +1,108 @@
-/**
- * @copyright   Copyright (C) 2005 - 2018 Open Source Matters, Inc. All rights reserved.
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
+/*
+ * @package Joomla
+ * @copyright Copyright (C) 2005 Open Source Matters. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ *
+ * @extension Phoca Filter Options System Plugin
+ * @copyright Copyright (C) Jan Pavelka www.phoca.cz
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
  
 document.addEventListener("DOMContentLoaded", () => { 
   	
-	function filterOptionsQuote(str) {
+	function phFilterOptionsQuote(str) {
 		return str.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, "\\$&" );
 	};
+
+	function phRemoveElementsByClass(parentElem, className){
+		var elements = parentElem.getElementsByClassName(className);
+		while(elements.length > 0){
+			elements[0].parentNode.removeChild(elements[0]);
+		}
+	}
 	
 	function filterOptions(str) {
 		
 		let hideElements 		= '.tab-pane .alert, .tab-pane .field-spacer, fieldset legend, #sendtestmail, .tab-description, .tab-pane h3, .ph-admin-additional-box';
 		let hideElementBoxes	= '.tab-header, .ph-options-head, .ph-options-head-expert';
-		let tab 				= '#configTabs li a, .nav-tabs li a';
-		let tabContent			= '#configContent div.tab-pane, #config-document div.tab-pane, .tab-content .tab-pane';
+		let tab 				= '#configTabs div button';
+		let tabContent			= '#configTabs > joomla-tab-element';
 		let excludeTabs			= '#page-filters, #page-permissions, #permissions, #permissions_label';
 		let itemParameter		= '.control-group .control-label label';
 		let itemParameterBox	= '.control-group';
 		let tabRow				= '.tab-pane .row div';
 		let tabFieldset			= '.tab-pane .row div fieldset';
 		
-		/* J39 */
-		tab 					= '#configTabs li, .nav-tabs li';
-		tabRow					= '.tab-pane .row-fluid div';
-		tabFieldset				= '.tab-pane .row-fluid div fieldset';
-		/**/
+
+		// Get active tab and return it back after filtering
+		let activeTab = false;
+		document.querySelectorAll(tabContent).forEach((elem) => {
+
+			/*if (elem.getAttribute('aria-hidden') == 'true') {
+				
+			} else {
+				activeTab = elem;
+			}*/
+			/*
+			* Unfortunately, every Joomla version changes dramatically and therefore this part must change as well 
+			* So this is in fact not a final solution
+			* Phoca Filter Options must set all tabs as active so when it does, then it is not possible to detect the original active tab
+			* This is why we differentiate between
+			* active="true" ... standard active tab set by system
+			* active="specific" ... active tab set by this plugin to be able to search all tabs
+			* In both cases, active is active for Joomla core system ... FOR NOW
+			*/
+
+			if (elem.hasAttribute('active') && elem.getAttribute('active') != 'specific') {
+				activeTab = elem;
+			}
+			
+		});
+	
+
 		
 		if (str.length > 0) {
 			
 			/* Make tabs disabled */
 			document.querySelectorAll(tab).forEach((elem) => {
-				/* J4 elem.classList.add('disabled'),
-				elem.classList.remove('active'),
-				elem.classList.remove('show')*/
-				elem.classList.add('active')
+				elem.classList.add('disabled');
+				elem.classList.add('phFilterOptionsDisabled');
+
 			});
+
 			/* Make all tab contents active so they can be searched */
-			document.querySelectorAll(tabContent).forEach((elem) => {elem.classList.add('active')});
+			document.querySelectorAll(tabContent).forEach((elem) => {
+
+				//elem.setAttribute('active', true);
+				if (!elem.hasAttribute('active')){
+					elem.setAttribute('active', "specific");
+				}
+				elem.classList.add('phFilterOptionsNoStyle');
+				phRemoveElementsByClass(elem, 'phFilterOptionsHeader');
+
+				// Add tab information (yellow box displayed in filter results)
+				var newItem = document.createElement("div");
+				newItem.classList.add('phFilterOptionsHeader');
+				var textnode = document.createTextNode(elem.getAttribute('name'));
+				newItem.appendChild(textnode); 
+				elem.insertBefore(newItem, elem.childNodes[0]);
+
+			});
+			
 			/* Hide some specific parts which are not neccessary to display in search results */
-			document.querySelectorAll(hideElements).forEach((elem) => {elem.style.display = "none";});
+			document.querySelectorAll(hideElements).forEach((elem) => {
+				elem.classList.add('phFilterOptionsHidden');
+				elem.parentElement.classList.add('phFilterOptionsFieldset');
+			
+			});
 			/* Hide boxes of specific parts which are not neccessary to display in search results */
-			document.querySelectorAll(hideElementBoxes).forEach((elem) => {elem.parentElement.parentElement.style.display = "none";});
+			document.querySelectorAll(hideElementBoxes).forEach((elem) => {
+				elem.parentElement.parentElement.classList.add('phFilterOptionsHidden');
+			});
+			
 			/* Hide tab items which cannot be filtered */
 			document.querySelectorAll(excludeTabs).forEach((elem) => {
-				elem.classList.add('disabled'),
-				elem.classList.remove('active'),
-				elem.classList.remove('show')
+				elem.classList.add('disabled');
 			});
 			
 			
@@ -58,14 +114,16 @@ document.addEventListener("DOMContentLoaded", () => {
 			document.querySelectorAll(itemParameter).forEach((elem) => {
 				let ePP	= elem.parentElement.parentElement;
 				let item = elem.innerHTML;
-				ePP.style.display = "none";
+				//ePP.style.display = "none";
+				ePP.classList.add('phFilterOptionsHidden');
 				
 				if (item && typeof item == "string") {
-					let re = new RegExp(filterOptionsQuote(str), "i");
+					let re = new RegExp(phFilterOptionsQuote(str), "i");
 					let res = item.match(re);
 					
 					if (res) {
-						ePP.style.display = "block";
+						//ePP.style.display = "block";
+						ePP.classList.remove('phFilterOptionsHidden');
 					}
 				}
 			});
@@ -74,38 +132,38 @@ document.addEventListener("DOMContentLoaded", () => {
 		
 		/* Remove disabled class from all tabs */
 		document.querySelectorAll(tab).forEach((elem) => {
-			/* J4 elem.classList.remove('disabled'),
-			elem.classList.remove('active'),
-			elem.classList.remove('show')*/
-			elem.classList.remove('active')
+			elem.classList.remove('disabled');
+			elem.classList.remove('phFilterOptionsDisabled');
 		});
-		
-		/* Make first tab in active */
-		if(document.querySelector(tab)) {
-			document.querySelector(tab).classList.add('active');
-			document.querySelector(tab).classList.add('show');
-		}
 		
 		/* Remove active class from all tab contents */
 		document.querySelectorAll(tabContent).forEach((elem) => {
-			elem.classList.remove('active'),
-			elem.classList.remove('show')
+
+			elem.removeAttribute('active');
+			//elem.setAttribute("aria-hidden", "true");
+			elem.classList.remove('phFilterOptionsNoStyle');
+			phRemoveElementsByClass(elem, 'phFilterOptionsHeader');
+
 		});
-		
-		/* Make first tab CONTENT in global configuration options active */
-		if(document.querySelector(tabContent)) {
-			document.querySelector(tabContent).classList.add('active');
-			document.querySelector(tabContent).classList.add('show');
+
+		/* Return back the active tab */
+
+		if(activeTab) {
+			activeTab.setAttribute("active", true);
+			//activeTab.removeAttribute("aria-hidden");	
 		}
 		
 		/* Display all hidden parts back - undo the changes we've made when searching */
-		document.querySelectorAll(hideElements).forEach((elem) => {elem.style.display = "block";});
-		
+		document.querySelectorAll(hideElements).forEach((elem) => {
+			elem.classList.remove('phFilterOptionsHidden');
+			elem.parentElement.classList.remove('phFilterOptionsFieldset');
+		});
+
 		/* Display all hidden boxex of parts back - undo the changes we've made when searching */ 
-		document.querySelectorAll(hideElementBoxes).forEach((elem) => {elem.parentElement.parentElement.style.display = "block";});
+		document.querySelectorAll(hideElementBoxes).forEach((elem) => {elem.parentElement.parentElement.classList.remove('phFilterOptionsHidden');});
 		
 		/* Display all hidden paremter items back - undo the changes we've made when searching */
-		document.querySelectorAll(itemParameterBox).forEach((elem) => {elem.style.display = "block";});
+		document.querySelectorAll(itemParameterBox).forEach((elem) => {elem.classList.remove('phFilterOptionsHidden');});
 		
 		/* Display items which cannot be filtered - undo the changes we've made when searching */
 		document.querySelectorAll(excludeTabs).forEach((elem) => {elem.classList.remove('disabled')});
@@ -116,7 +174,6 @@ document.addEventListener("DOMContentLoaded", () => {
 		
 	}
 
-	
 	/* Events */
 	document.getElementById("filterOptionsClear").addEventListener("click", (event) => {
 		document.getElementById("filterOptionsInput").value = "";
--- phocafilteroptions.php
+++ phocafilteroptions.php
@@ -6,86 +6,92 @@
  * @copyright Copyright (C) Jan Pavelka www.phoca.cz
  * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
  */
+
+
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\HTML\HTMLHelper;
+
 defined( '_JEXEC' ) or die( 'Restricted access' );
 jimport( 'joomla.plugin.plugin' );
 
-class plgSystemPhocaFilteroptions extends JPlugin
-{	
-	
+class plgSystemPhocaFilteroptions extends CMSPlugin
+{
+
 	public function __construct(& $subject, $config) {
-		
+
 		$this->loadLanguage('plg_system_phocafilteroptions');
 		parent::__construct($subject, $config);
 	}
-	
+
 	public function isActive() {
-		
-		$app 					= JFactory::getApplication();
+
+		$app 					= Factory::getApplication();
 		$option 				= $app->input->get('option', '', 'string');
 		$view 					= $app->input->get('view', '', 'string');
 		$layout 				= $app->input->get('layout', '', 'string');
 		$component_edit_view 	= $this->params->get('component_edit_view', array());
-		
-		if ($app->getName() != 'administrator') { 
+
+		if ($app->getName() != 'administrator') {
 			return false;
 		}
-		
-		if ($option == 'com_config') { 
+
+		if ($option == 'com_config') {
 			return true;
 		}
-		
+
 		if ($layout == 'edit' && in_array($option, $component_edit_view)) {
 			return true;
 		}
-		
+
 		return false;
 
 	}
-	
-	
+
+
 	function onBeforeRender() {
-		
+
 		$active = $this->isActive();
-		
+
 		if (!$active) {
 			return '';
 		}
-		
-		// Add JS
-		$document	= JFactory::getDocument();
-		$document->addScript(JURI::root(true).'/media/plg_system_phocafilteroptions/js/config-filter-options.js');///
-		JHtml::stylesheet('media/plg_system_phocafilteroptions/css/filter-options.css' );
+
+		//HTMLHelper::_('script', 'media/plg_system_phocafilteroptions/js/config-filter-options.min.js', array('version' => 'auto'));
+		//HTMLHelper::_('script', 'media/plg_system_phocafilteroptions/js/config-filter-options.es6.js', array('version' => 'auto'));
+		HTMLHelper::_('script', 'media/plg_system_phocafilteroptions/js/config-filter-options.es5.min.js', array('version' => 'auto'));
+		HTMLHelper::_('stylesheet', 'media/plg_system_phocafilteroptions/css/filter-options.css', array('version' => 'auto'));
+
 		return true;
 	}
-	
+
 	function onAfterRender() {
-		
+
 		$active = $this->isActive();
-		
+
 		if (!$active) {
 			return '';
 		}
-		
-		$app 					= JFactory::getApplication();
-		$option 				= $app->input->get('option', '', 'string');
-		
+
+		$app 	= Factory::getApplication();
+		$option = $app->input->get('option', '', 'string');
+
 		// Add HTML
-		$buffer = JResponse::getBody();
-		
+		$buffer = $app->getBody();
+
 		if ($option == 'com_config') {
-			$addHtml = '<div class="row-fluid ph-filter-options config">'
-					  .'<div class="span2"></div>'
-					  .'<div class="span10"><form class="form-inline"><input type="text" id="filterOptionsInput" placeholder="'.JText::_('PLG_SYSTEM_PHOCAFILTEROPTIONS_TYPE_FILTER_OPTIONS').'" /> <button type="button" id="filterOptionsClear" class="btn btn-primary" title="'. JText::_('JSEARCH_FILTER_CLEAR').'">'.JText::_('JSEARCH_FILTER_CLEAR').'</button></form></div>'
+			$addHtml = '<div class="ph-filter-options config">'
+					  .'<form class="form-inline"><input class="form-control" type="text" id="filterOptionsInput" placeholder="'.JText::_('PLG_SYSTEM_PHOCAFILTEROPTIONS_TYPE_FILTER_OPTIONS').'" /> <button type="button" id="filterOptionsClear" class="btn btn-primary" title="'. JText::_('JSEARCH_FILTER_CLEAR').'">'.JText::_('JSEARCH_FILTER_CLEAR').'</button></form>'
 				  .'</div>';
 		} else {
 			$addHtml = '<div class="ph-filter-options component '.$option.'">'
 					  .'<form class="form-inline"><input type="text" id="filterOptionsInput" placeholder="'.JText::_('PLG_SYSTEM_PHOCAFILTEROPTIONS_TYPE_FILTER_OPTIONS').'" /> <button type="button" id="filterOptionsClear" class="btn btn-primary" title="'. JText::_('JSEARCH_FILTER_CLEAR').'">'.JText::_('JSEARCH_FILTER_CLEAR').'</button></form>'
 				  .'</div>';
-		}	  
-		
+		}
+
 		// Use the easiest and quickest replace method
 		$buffer	= str_replace("<form action=", $addHtml . "<form action=", $buffer);
-		JResponse::setBody($buffer);
+		$app->setBody($buffer);
 
 		return true;
 	}
